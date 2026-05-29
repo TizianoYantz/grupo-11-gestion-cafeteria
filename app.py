2b5d06db1a6f7059ef5d0dc50a58cbc545f25ca9
Martina <martina.s.barabas@hotmail.com>
2026-05-29 11:48:58 -0300

    Configuro Home como pantalla inicial y separo CSS

--- app.py
+++ app.py
@@ -4,26 +4,17 @@ from modelos.producto import Producto
 from modelos.venta import Venta
 from modelos.inventario import Inventario
 
-
 app = Flask(__name__)
 
+# HOME como pantalla inicial
 @app.route("/")
-def index():
-
-    productos = Producto.listar_productos()
-
-    return render_template(
-        "index.html",
-        productos=productos
-    )
+def home():
+    return render_template("Home.html")
 
 # CREAR PRODUCTO
-
 @app.route("/crear", methods=["GET", "POST"])
 def crear_producto():
-
     if request.method == "POST":
-
         nombre = request.form["nombre"]
         categoria = request.form["categoria"]
         precio = request.form["precio"]
@@ -35,21 +26,21 @@ def crear_producto():
             precio,
             stock
         )
-
         nuevo_producto.crear_producto()
-
-        return redirect("/")
+        return redirect("/inventario")
 
     return render_template("crear_producto.html")
 
-# EDITAR PRODUCTO
+# INVENTARIO
+@app.route("/inventario")
+def inventario():
+    productos = Producto.listar_productos()
+    return render_template("index.html", productos=productos)
 
+# EDITAR PRODUCTO
 @app.route("/editar/<int:id>", methods=["GET", "POST"])
 def editar_producto(id):
-
-    # SI ENVÍA FORMULARIO
     if request.method == "POST":
-
         nombre = request.form["nombre"]
         categoria = request.form["categoria"]
         precio = request.form["precio"]
@@ -61,25 +52,17 @@ def editar_producto(id):
             precio,
             stock
         )
-
         producto.editar_producto(id)
-
-        return redirect("/")
+        return redirect("/inventario")
 
     producto = Producto.obtener_producto(id)
-
-    return render_template(
-        "editar_producto.html",
-        producto=producto
-    )
+    return render_template("editar_producto.html", producto=producto)
 
 # ELIMINAR PRODUCTO
-
 @app.route("/eliminar/<int:id>", methods=["POST"])
 def eliminar_producto(id):
-
     Producto.eliminar_producto(id)
+    return redirect("/inventario")
 
-    return redirect("/")
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
